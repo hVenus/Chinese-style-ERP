f351c3fd095931447ed7dba64135d17ca485f09f
hVenus <kevin@fm618.com>
2013-08-19 00:53:32 +0800

    module

---
+++ general/inventory/index.php
@@ -0,0 +1,10 @@
+<?php 
+include_once('inc/auth.php');
+?>
+<html>
+<head></head>
+
+<body>
+	<h1>inventory</h1>
+</body>
+</html>
---
+++ general/sale/index.php
@@ -0,0 +1,10 @@
+<?php 
+include_once('inc/auth.php');
+?>
+<html>
+<head></head>
+
+<body>
+<h1>SALE</h1>
+</body>
+</html>
